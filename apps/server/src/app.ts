fc55674902bef642be64971ee32f5ff2754e802b
David Uzondu <uzondudavid2004@gmail.com>
2026-04-27 17:13:09 +0100

    feat(auth): add auth routes

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -1,46 +1,60 @@
-import '@/scripts/seed-db';
+import "@/scripts/seed-db";
 import { env } from "@hng-i14-task-0-david-uzondu/env/server";
-import express, { type Express, type NextFunction, type Request, type Response } from "express";
+import express, {
+  type Express,
+  type NextFunction,
+  type Request,
+  type Response,
+} from "express";
 import cors from "cors";
 import type { ErrorResponse } from "@/misc/types";
-import profileRoutes from '@/modules/profile/profile.route';
+import profileRoutes from "@/modules/profile/profile.route";
+import authRoutes from "@/modules/auth/auth.route";
 import { AppError } from "@/errors/app.error";
 
 const app: Express = express();
 app.use(
- cors({
-  origin: env.CORS_ORIGIN,
-  methods: ["GET", "POST", "OPTIONS"],
- }),
+  cors({
+    origin: env.CORS_ORIGIN,
+    methods: ["GET", "POST", "OPTIONS"],
+  }),
 );
 
 app.use(express.json());
 
 app.get("/", (_req, res) => {
- res.status(200).send("OK world");
+  res.status(200).send("OK world");
 });
 
 app.use("/api/profiles", profileRoutes);
+app.use("/auth", authRoutes);
 
-app.use((err: Error, _req: Request, res: Response<ErrorResponse>, _next: NextFunction) => {
- console.error(err)
- if (err instanceof AppError) {
-  return res.status(err.code).json({
-   status: 'error',
-   message: err.message
-  })
- }
- if (err instanceof SyntaxError && 'body' in err) {
-  return res.status(400).json({
-   status: "error",
-   message: err.message
-  });
- } else {
-  return res.status(500).json({
-   status: "error",
-   message: "Internal server error"
-  })
- }
-})
+app.use(
+  (
+    err: Error,
+    _req: Request,
+    res: Response<ErrorResponse>,
+    _next: NextFunction,
+  ) => {
+    console.error(err);
+    if (err instanceof AppError) {
+      return res.status(err.code).json({
+        status: "error",
+        message: err.message,
+      });
+    }
+    if (err instanceof SyntaxError && "body" in err) {
+      return res.status(400).json({
+        status: "error",
+        message: err.message,
+      });
+    } else {
+      return res.status(500).json({
+        status: "error",
+        message: "Internal server error",
+      });
+    }
+  },
+);
 
 export default app;
---
+++ apps/server/src/modules/auth/auth.route.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+
+const router: Router = Router();
+
+router.get("/github", () => {});
+router.get("/github/callback")
+router.post("/refresh")
+router.post("/logout")
+
+export default router;
