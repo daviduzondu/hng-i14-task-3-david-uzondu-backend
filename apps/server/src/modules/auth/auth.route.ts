e403de1dab162e8e3fc49d523bcbaaf94a6af241
David Uzondu <uzondudavid2004@gmail.com>
2026-04-29 03:38:18 +0100

    chore: replace `client_id` with environment variable

--- apps/server/src/modules/auth/auth.route.ts
+++ apps/server/src/modules/auth/auth.route.ts
@@ -11,13 +11,12 @@ router.get("/github", async (req, res, next) => {
   const pkce = await pkceChallenge();
   const state = uuidv4();
   res.redirect(
-    `https://github.com/login/oauth/authorize?client_id=Ov23liXhC3Msu8ermI7j&redirect_uri=${process.env.BACKEND_URL}/auth/github/callback&state=${state}&code_challenge=${pkce.code_challenge}&code_challenge_method=${pkce.code_challenge_method}&scope=read:user,user:email`,
+    `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_OAUTH_CLIENT_ID}&redirect_uri=${process.env.BACKEND_URL}/auth/github/callback&state=${state}&code_challenge=${pkce.code_challenge}&code_challenge_method=${pkce.code_challenge_method}&scope=read:user,user:email`,
   );
 });
 router.get(
   "/github/callback",
   validateSchema(githubCallbackSchema, (req) => req.query),
-  //   (req) => {validateSchema(githubCallbackSchema, req.body)},
   authController.createUser,
 );
 router.post("/refresh", authController.refreshToken);
